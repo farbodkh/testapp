88ec74688dced21354e5786c1c5e1ffa72a0e868
Farbod Khamooshi <farbodkhamooshi@farbod-khamooshis-mac-book-pro.local>
2017-01-14 19:09:06 +0400

    Initial Commit

---
+++ testapp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testapp
+//
+//  Created by Farbod Khamooshi on 14/1/17.
+//  Copyright © 2017 Farbod Khamooshi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
